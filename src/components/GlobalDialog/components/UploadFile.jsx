6a485b112d367a8b92ac9c5ff7b720847e329a8b
bhupendp <Bhupendra.Patel@amdocs.com>
2024-03-20 17:11:05 +0200

    Added File extensions in the Input file

--- src/components/GlobalDialog/components/UploadFile.jsx
+++ src/components/GlobalDialog/components/UploadFile.jsx
@@ -9,11 +9,11 @@ const UploadFile = ({handleFileChange, handleFileUpload, loading, handleOnClose}
                     Upload
                 </h2>
                 <div className={'flex flex-col pt-5 px-5'}>
-                    <input type='file' multiple name='sourses' onChange={handleFileChange}/>
+                    <input type='file' accept='.pdf,.txt,.zip,.doc' multiple name='sourses' onChange={handleFileChange}/>
                 </div>
                 <div className="px-4 py-4 mt-2 flex justify-end shadow-2xl">
                     <button onClick={handleFileUpload}
-                            className={`rounded font-semibold justify-center align-middle content-center text-2xl px-4 mx-3 text-transparent bg-gradient-to-r from-yellow-400 to-pink-700 bg-clip-text`}>
+                        className={`rounded font-semibold justify-center align-middle content-center text-2xl px-4 mx-3 text-transparent bg-gradient-to-r from-yellow-400 to-pink-700 bg-clip-text`}>
                         {loading ? <Loader size={50}/> : 'Upload'}
                     </button>
                     <button onClick={handleOnClose} className="rounded font-semibold text-2xl text-black">
